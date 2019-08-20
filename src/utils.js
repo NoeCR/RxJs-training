e755c2d259d5b32e314446924057439d1d47a5a5
NoeCR <noecruzrodriguez@gmail.com>
2019-08-20 21:39:46 +0200

    Game Tic Tac Toe created only used Observables for your execution

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
 /* Utility to show content into the view */
-export const displayLog = (content) => {
+export const displayLog = (content)=> {
     let element = document.createElement('div');
     element.innerHTML = content;
     const logContainer = document.getElementById("log-container");
     logContainer.appendChild(element);
-}
-export const resetLog = () => {
-    const logContainer = document.getElementById("log-container").innerHTML = '';
 }
